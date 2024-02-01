461d8eafd277b5219e1b677ed0aaeb3c04c8bb5b
Myungseok Kang <dooly6276@gmail.com>
2024-02-01 21:52:03 +0900

    Initial Commit

---
+++ src/main/java/com/example/timepostit/TimePostitApplication.java
@@ -0,0 +1,13 @@
+package com.example.timepostit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimePostitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimePostitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/timepostit/TimePostitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timepostit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimePostitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
