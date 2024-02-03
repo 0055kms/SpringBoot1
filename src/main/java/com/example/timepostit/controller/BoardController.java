6aba926b014be69ae41b2f2a7e2d4fcecbf3373b
Myungseok Kang <dooly6276@gmail.com>
2024-02-03 18:46:16 +0900

    index.mustache, show.mustache

--- src/main/java/com/example/timepostit/controller/BoardController.java
+++ src/main/java/com/example/timepostit/controller/BoardController.java
@@ -6,8 +6,14 @@ import com.example.timepostit.repository.BoardRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @Controller
 public class BoardController {
@@ -26,5 +32,17 @@ public class BoardController {
         log.info(saved.toString());
         return "";
     }
-
+    @GetMapping("/boards/{id}")
+    public String showBoard(@PathVariable Long id, Model model){
+        Board board = boardRepository.findById(id).orElse(null);
+        model.addAttribute("board",board);
+        return "boards/show";
+    }
+    @GetMapping("/boards")
+    public String showIndex(Model model){
+        List<Board> boardList = boardRepository.findAll();
+        log.info(boardList.toString());
+        model.addAttribute("boardList",boardList);
+        return "boards/index";
+    }
 }
